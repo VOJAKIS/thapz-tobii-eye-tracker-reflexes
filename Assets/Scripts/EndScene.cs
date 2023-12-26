ffe0c16cd65e457c68d98c87262e2f02634f0094
VOJAKIS <valastan1@gmail.com>
2023-12-27 00:28:10 +0100

    Finishing touches

--- Assets/Scripts/EndScene.cs
+++ Assets/Scripts/EndScene.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
@@ -8,38 +9,42 @@ public class EndScene : MonoBehaviour
 {
 	public TMP_Text endReactionsText;
 
+	public Button playAgainButton;
+	public Button goBackToMainMenuButton;
+
 	ReactionTime reactionTime;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		goBackToMainMenuButton.onClick.AddListener(ChangeSceneToMainMenu);
+		playAgainButton.onClick.AddListener(ChangeSceneToGame);
 	}
 
 	// Update is called once per frame
 	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			SceneManager.LoadScene("Game");
-		}
+	{ }
 
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
-			SceneManager.LoadScene("MainMenu");
-		}
+	void ChangeSceneToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	void ChangeSceneToGame()
+	{
+		SceneManager.LoadScene("Game");
 	}
 
 	void OnEnable()
 	{
 		reactionTime = DataSaver.loadData<ReactionTime>("reactionTime");
 
-		string reactionTimes = "REACTION TIMES";
+		// string reactionTimes = "REACTION TIMES";
 		string numberOfPoints = "number of points: " + reactionTime.getCount().ToString("F0");
 		string slowestReactionTime = "slowest: " + reactionTime.getMaximum().ToString("F0") + "ms";
 		string averageReactionTime = "average: " + reactionTime.getAverage().ToString("F0") + "ms";
 		string fastestReactionTime = "fastest: " + reactionTime.getMinimum().ToString("F0") + "ms";
 
-		endReactionsText.text = reactionTimes + "\n\n" + numberOfPoints + "\n" + slowestReactionTime + "\n" + averageReactionTime + "\n" + fastestReactionTime;
+		endReactionsText.text = numberOfPoints + "\n" + slowestReactionTime + "\n" + averageReactionTime + "\n" + fastestReactionTime;
 	}
 }
